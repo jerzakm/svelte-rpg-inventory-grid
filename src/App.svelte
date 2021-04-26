fc43d330c627323afc0a01de5dc0abc5aaf21865
Marcin J <jerzakm@gmail.com>
2021-04-26 10:39:32 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Grid from "./components/Grid.svelte";
+</script>
+
+<main>
+  <div class="grid-container">
+    <Grid width="12" height="16" itemSize="32" />
+  </div>
+</main>
+
+<style>
+  .grid-container {
+    margin: 1rem;
+    display: flex;
+  }
+</style>
---
+++ src/components/Grid.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let width;
+  export let height;
+  export let itemSize;
+</script>
+
+<grid style={`grid-template-columns: repeat(${width}, ${itemSize}px);`}>
+  {#each { length: width } as i, x}
+    {#each { length: height } as j, y}
+      <square style={`width:${itemSize}px; height: ${itemSize}px;`}>s</square>
+    {/each}
+  {/each}
+</grid>
+
+<style>
+  grid {
+    display: grid;
+    border: 1px solid black;
+  }
+  square {
+    display: flex;
+  }
+</style>
